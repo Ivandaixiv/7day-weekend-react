984e132d1f8ef52f1aae36ed4b454358909d0dad
Ivan <ivandaixivwork@gmail.com>
2020-04-27 16:00:58 -0700

    Adds profile for Shane Carling

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -6,10 +6,7 @@ const Home = (props) => {
   return (
     <>
       <div className={classes.heroContainer}>
-        <img
-          src="https://via.placeholder.com/1920x600"
-          alt="7 Day Weekend Logo"
-        />
+        <img src="https://via.placeholder.com/1920x600" alt="Banner" />
         <Button>Call to Action Button</Button>
       </div>
       <h2>Our Mission/Goal</h2>
@@ -22,6 +19,21 @@ const Home = (props) => {
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
       </p>
+      <div>
+        <img src="https://via.placeholder.com/500" alt="Shane Carling" />
+        <div>
+          <h2>Shane Carling</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
