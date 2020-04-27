6bc830449c861c11af11633857d87c69546fe96d
Ivan <ivandaixivwork@gmail.com>
2020-04-27 14:17:32 -0700

    Updates

---
+++ precache-manifest.fa0f22798296155f0843db59569e8a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28905f1520004d518d15515c8ee7d8e9",
+    "url": "/7day-weekend-react/index.html"
+  },
+  {
+    "revision": "931ae3c56b9f5259749f",
+    "url": "/7day-weekend-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c8a70bc424d4dfad97f0",
+    "url": "/7day-weekend-react/static/js/2.8598a8f4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/7day-weekend-react/static/js/2.8598a8f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "931ae3c56b9f5259749f",
+    "url": "/7day-weekend-react/static/js/main.7e1fc6f0.chunk.js"
+  },
+  {
+    "revision": "ee015736395281abefc1",
+    "url": "/7day-weekend-react/static/js/runtime-main.7764c622.js"
+  }
+]);
