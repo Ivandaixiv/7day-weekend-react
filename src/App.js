ddd4db2b654f634d566cfc3d080a838733fe911f
Ivan <ivandaixivwork@gmail.com>
2020-04-29 17:05:01 -0700

    Adds a footer component to the UI

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <Router>
         <AppRoutes />
+        <Footer />
       </Router>
     </div>
   );
