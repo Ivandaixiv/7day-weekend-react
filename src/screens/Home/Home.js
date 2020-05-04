1325cddf419c297f3f73d4014cab4e1dc385aac1
Ivan <ivandaixivwork@gmail.com>
2020-05-04 16:02:33 -0700

    Adds styling to the introduction paragraphs

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -9,21 +9,23 @@ const Home = (props) => {
         <img src="https://via.placeholder.com/1920x600" alt="Banner" />
         <Button>Call to Action Button</Button>
       </div>
-      <h2>Our Mission/Goal</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <div>
+      <div className={classes.paragraphContainer}>
+        <h2 className={classes.header}>Our Mission/Goal</h2>
+        <p className={classes.paragraph}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div className={classes.imagePContainer}>
         <img src="https://via.placeholder.com/500" alt="Shane Carling" />
-        <div>
-          <h2>Shane Carling</h2>
-          <p>
+        <div className={classes.paragraphContainer}>
+          <h2 className={classes.header}>Shane Carling</h2>
+          <p className={classes.paragraph}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -12,6 +12,25 @@ const styles = () =>
         transform: "translate(0, -75px)",
       },
     },
+    paragraphContainer: {
+      textAlign: "left",
+      display: "flex",
+      flexDirection: "column",
+      width: "100%",
+    },
+    header: {
+      alignSelf: "center",
+    },
+    paragraph: {
+      alignSelf: "center",
+      width: "30vw",
+    },
+    imagePContainer: {
+      display: "flex",
+      "& div": {
+        justifyContent: "center",
+      },
+    },
   });
 
 export default styles;
